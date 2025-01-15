c9c22efa0f30e65fde3fb3849262b16e949ace18
Nelson drew <nelsondrew07@gmail.com>
2025-01-16 01:52:15 +0530

    update base url

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://nodejs-gcp-app-kr63km7dxa-uc.a.run.app'
 
 export async function sendOtp(phoneNumber, simulateStage) {
     const url = `${BASE_URL}/send-otp`;
